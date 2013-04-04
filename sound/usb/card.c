f9d28ca942e118d66a834e87e06b91ff406cbd62
Clemens Ladisch <clemens@ladisch.de>
2013-04-04 22:31:05 +0200

    ALSA: usb-audio: update debugging output
    Interface status is no longer needed.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -532,7 +532,6 @@ static int usb_audio_probe(struct usb_interface *intf,
 	chip->probing = 0;
 	usb_set_intfdata(intf, chip);
 	mutex_unlock(&register_mutex);
-	printk(KERN_ERR "intf %d: registered\n", ifnum);
 	return 0;
 
  __error:
@@ -543,7 +542,6 @@ static int usb_audio_probe(struct usb_interface *intf,
 	}
 	mutex_unlock(&register_mutex);
  __err_val:
-	printk(KERN_ERR "intf %d: failed: %d\n", ifnum, err);
 	return err;
 }
 
@@ -712,7 +710,7 @@ static int __init snd_usb_audio_init(void)
 		printk(KERN_WARNING "invalid nrpacks value.\n");
 		return -EINVAL;
 	}
-	printk(KERN_INFO "usb-audio debug test 5\n");
+	printk(KERN_INFO "usb-audio debug test 6\n");
 	return usb_register(&usb_audio_driver);
 }
 
